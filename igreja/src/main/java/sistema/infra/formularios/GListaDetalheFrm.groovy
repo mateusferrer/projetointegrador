4a730035ba0bd25e663c1fec75952281a2254b88
Francisquini <Francisquini@192.168.25.106>
2016-05-10 13:59:45 -0300

    Alterações 10/05/2016

--- igreja/src/main/java/sistema/infra/formularios/GListaDetalheFrm.groovy
+++ igreja/src/main/java/sistema/infra/formularios/GListaDetalheFrm.groovy
@@ -2,7 +2,7 @@ package sistema.infra.formularios
 
 import com.forj.cirrus.infra.exceptions.NegocioException
 import com.forj.cirrus.jsf.gui.AbstractListaDetalheFrm
-import com.forj.cirrus.negocio.dominio.servicos.Dominio;
+import com.forj.cirrus.negocio.dominio.servicos.Dominio
 import com.forj.cirrus.util.Bean
 
 /**
@@ -12,71 +12,71 @@ import com.forj.cirrus.util.Bean
  * @since 01/12/2015
  */class GListaDetalheFrm<T extends Dominio> extends AbstractListaDetalheFrm<T> {
 
-    /** Armazena a navegação JSF . **/
-    static String LISTA = "lista.xhtml"
+	/** Armazena a navegação JSF . **/
+	static String LISTA = "lista.xhtml"
 
-    /** Armazena a navegação JSF . **/
-    static String DETALHE = "detalhe.xhtml"
+	/** Armazena a navegação JSF . **/
+	static String DETALHE = "detalhe.xhtml"
 
-    def dominio;
+	protected Object dominio;
 
-    /** {@inheritDoc} */
-    @Override
-    void entrar() {
-        colecao.clear()
-        dominio.limpar()
-    }
+	/** {@inheritDoc} */
+	@Override
+	void entrar() {
+		colecao.clear()
+		dominio.limpar()
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    String novo() {
-        dominio.limpar()
-        alterar = false
-        DETALHE
-    }
+	/** {@inheritDoc} */
+	@Override
+	String novo() {
+		dominio.limpar()
+		alterar = false
+		DETALHE
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    String alterar(T objeto) {
-        Bean.copiarPropriedades(dominio, objeto)
-        alterar = true
-        DETALHE
-    }
+	/** {@inheritDoc} */
+	@Override
+	String alterar(T objeto) {
+		Bean.copiarPropriedades(dominio, objeto)
+		alterar = true
+		DETALHE
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    String cancelar() {
-        dominio.limpar()
-        LISTA
-    }
+	/** {@inheritDoc} */
+	@Override
+	String cancelar() {
+		dominio.limpar()
+		LISTA
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    void deletar(T objeto) {
-        try {
-            bean.excluir(objeto)
-            colecao.remove(objeto)
-        } catch (ex) {
-            gerarErrosRia(ex)
-        }
-    }
+	/** {@inheritDoc} */
+	@Override
+	void deletar(T objeto) {
+		try {
+			bean.excluir(objeto)
+			colecao.remove(objeto)
+		} catch (ex) {
+			gerarErrosRia(ex)
+		}
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    String gravar() {
-        try {
-            if (alterar) {
-                bean.alterar(dominio)
-                alterar = false
-                atualizarLista(dominio)
-                dominio.limpar()
-            } else {
-                bean.inserir(dominio)
-                dominio.limpar()                gerarMensagemRia("Cadastro realizado com sucesso.")
-            }
-        } catch (ex) {
-            gerarErrosRia(ex)
-        }
-        DETALHE
-    }
+	/** {@inheritDoc} */
+	@Override
+	String gravar() {
+		try {
+			if (alterar) {
+				bean.alterar(dominio)
+				alterar = false
+				atualizarLista(dominio)
+				dominio.limpar()
+			} else {
+				bean.inserir(dominio)
+				dominio.limpar()				gerarMensagemRia("Cadastro realizado com sucesso.")
+			}
+		} catch (ex) {
+			gerarErrosRia(ex)
+		}
+		DETALHE
+	}
 }
