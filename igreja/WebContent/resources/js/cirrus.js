9d93e71db1df2805d345eb60c8cbb76a0fab929d
lfrancisquini <lfrancisquini@SUP75.unicred-parana.corp>
2016-05-27 17:35:47 -0300

    ~/

--- igreja/WebContent/resources/js/cirrus.js
+++ igreja/WebContent/resources/js/cirrus.js
@@ -48,8 +48,6 @@ function eventoClickBotao(data, botao, id) {
 	}
 }
 
-/* ViaCep */
-
 function limpa_formulário_cep() {
 	// Limpa valores do formulário de cep.
 	document.getElementById('rua').value = ("");
@@ -75,28 +73,39 @@ function meu_callback(conteudo) {
 	}
 }
 
-function pesquisacep(valor) {
+function pesquisacep() {
+
+	var valor = document.getElementById('cep');
+
 	// Nova variável "cep" somente com dígitos.
 	var cep = valor.replace(/\D/g, '');
+
 	// Verifica se campo cep possui valor informado.
 	if (cep != "") {
+
 		// Expressão regular para validar o CEP.
 		var validacep = /^[0-9]{8}$/;
+
 		// Valida o formato do CEP.
 		if (validacep.test(cep)) {
+
 			// Preenche os campos com "..." enquanto consulta webservice.
 			document.getElementById('rua').value = "...";
 			document.getElementById('bairro').value = "...";
 			document.getElementById('cidade').value = "...";
 			document.getElementById('uf').value = "...";
 			document.getElementById('ibge').value = "...";
+
 			// Cria um elemento javascript.
 			var script = document.createElement('script');
+
 			// Sincroniza com o callback.
 			script.src = '//viacep.com.br/ws/' + cep
 					+ '/json/?callback=meu_callback';
+
 			// Insere script no documento e carrega o conteúdo.
 			document.body.appendChild(script);
+
 		} // end if.
 		else {
 			// cep é inválido.
@@ -108,7 +117,7 @@ function pesquisacep(valor) {
 		// cep sem valor, limpa formulário.
 		limpa_formulário_cep();
 	}
-}
+};
 
 function uppercase(z) {
 	z.value = z.value.toUpperCase();
--- igreja/src/main/java/sistema/infra/converter/EntidadeConverter.java
+++ igreja/src/main/java/sistema/infra/converter/EntidadeConverter.java
@@ -10,42 +10,40 @@ import sistema.negocio.aplicacao.entidade.EntidadeBean;
 import sistema.negocio.dominio.entidade.Entidade;
 
 import com.forj.cirrus.infra.exceptions.NegocioException;
+import com.forj.cirrus.util.validacao.Val;
 
 /**
  * {Descrita resumida sobre a classe}.
- * 
  * @version 1.0 - 04/05/2016
  * @since 04/05/2016
  */
-@FacesConverter("igrejaConverter")
+@FacesConverter("entidadeConverter")
 public class EntidadeConverter implements Converter {
 
-	@Inject
-	protected EntidadeBean bean;
+    @Inject
+    protected EntidadeBean bean;
 
-	@Override
-	public Object getAsObject(FacesContext context, UIComponent component,
-			String value) {
-		Entidade retorno = null;
-		if (value != null) {
-			try {
-				retorno = bean.getPorCodigo(new Long(value));
-			} catch (NumberFormatException | NegocioException e) {
-				e.printStackTrace();
-			}
-		}
-		return retorno;
-	}
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        Entidade retorno = null;
+        if (!Val.vazio(value)) {
+            try {
+                retorno = bean.getPorCodigo(new Long(value));
+            } catch (NumberFormatException | NegocioException e) {
+                e.printStackTrace();
+            }
+        }
+        return retorno;
+    }
 
-	@Override
-	public String getAsString(FacesContext context, UIComponent component,
-			Object value) {
-		if (value != null) {
-			Long codigo = ((Entidade) value).getId();
-			String retorno = (codigo == null ? null : codigo.toString());
-			return retorno;
-		}
-		return "";
-	}
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (!Val.vazio(value)) {
+            Long codigo = ((Entidade) value).getId();
+            String retorno = (codigo == null ? null : codigo.toString());
+            return retorno;
+        }
+        return "";
+    }
 
 }
